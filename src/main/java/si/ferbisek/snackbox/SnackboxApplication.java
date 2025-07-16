ea47e9e32e936f701a87aa1bb5642b209b7d3f0f
Goran Ferbišek <goran.ferbisek@gmail.com>
2025-07-17 00:32:24 +0200

    Project generated with Spring initializr

---
+++ src/main/java/si/ferbisek/snackbox/SnackboxApplication.java
@@ -0,0 +1,13 @@
+package si.ferbisek.snackbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnackboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnackboxApplication.class, args);
+	}
+
+}
