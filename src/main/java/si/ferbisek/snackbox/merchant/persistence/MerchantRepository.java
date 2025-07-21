dc9382ec665a74b4777fcdc5d39358f396f6fbb4
Goran Ferbišek <goran.ferbisek@gmail.com>
2025-07-21 17:47:46 +0200

    Enable result paging for merchants

--- src/main/java/si/ferbisek/snackbox/merchant/persistence/MerchantRepository.java
+++ src/main/java/si/ferbisek/snackbox/merchant/persistence/MerchantRepository.java
@@ -1,8 +1,10 @@
 package si.ferbisek.snackbox.merchant.persistence;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MerchantRepository extends CrudRepository<MerchantEntity, Long> {
+public interface MerchantRepository extends CrudRepository<MerchantEntity, Long>,
+        PagingAndSortingRepository<MerchantEntity, Long> {
 }
