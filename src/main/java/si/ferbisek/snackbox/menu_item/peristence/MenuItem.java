0e4b0c9053457098003474f90c0e616fbaf94468
Goran Ferbišek <goran.ferbisek@gmail.com>
2025-08-21 10:24:33 +0200

    Create all entity relationships

---
+++ src/main/java/si/ferbisek/snackbox/menu_item/peristence/MenuItem.java
@@ -0,0 +1,39 @@
+package si.ferbisek.snackbox.menu_item.peristence;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import si.ferbisek.snackbox.menu_section.persistence.MenuSection;
+import si.ferbisek.snackbox.merchant.persistence.Merchant;
+
+import java.math.BigDecimal;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "menu_item")
+public class MenuItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "section_id")
+    private MenuSection section;
+
+    @ManyToOne
+    @JoinColumn(name = "merchant_id")
+    private Merchant merchant;
+
+
+
+    private BigDecimal price;
+
+}
