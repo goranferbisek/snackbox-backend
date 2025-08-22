798eac48654c5b4d8a359ef3690fb6afdffd3a34
Goran Ferbišek <goran.ferbisek@gmail.com>
2025-08-22 22:33:03 +0200

    WIP Implement category CRUD

---
+++ src/main/java/si/ferbisek/snackbox/category/persistence/CategoryRepository.java
@@ -0,0 +1,10 @@
+package si.ferbisek.snackbox.category.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    boolean existsByNameIgnoreCase(String name);
+}
