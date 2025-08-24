95dc4430f14fcaf7f0ad4e9ab4f1f90ce49ba1cd
Goran Ferbišek <goran.ferbisek@gmail.com>
2025-08-24 09:59:09 +0200

    Implement generating JWT tokens

---
+++ src/main/java/si/ferbisek/snackbox/security/AuthResponse.java
@@ -0,0 +1,15 @@
+package si.ferbisek.snackbox.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthResponse {
+    private String token;
+    private long expiresIn;
+}
---
+++ src/main/java/si/ferbisek/snackbox/security/service/AuthenticationService.java
@@ -0,0 +1,9 @@
+package si.ferbisek.snackbox.security.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface AuthenticationService {
+    UserDetails authenticate(String username, String password);
+
+    String generateToken(UserDetails userDetails);
+}
