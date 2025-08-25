f56eb240c1308a6d580a4129da6e66cfc990b945
Goran Ferbišek <goran.ferbisek@gmail.com>
2025-08-25 18:52:35 +0200

    Fix - enable auditing due to mandatory createdAt filed

--- src/main/java/si/ferbisek/snackbox/SnackboxApplication.java
+++ src/main/java/si/ferbisek/snackbox/SnackboxApplication.java
@@ -2,8 +2,10 @@ package si.ferbisek.snackbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SnackboxApplication {
 
 	public static void main(String[] args) {
