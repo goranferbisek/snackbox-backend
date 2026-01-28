adc3656ec833e0ee7fa54c7128178720e7e16894
Goran Ferbišek <goran.ferbisek@gmail.com>
2026-01-28 11:10:34 +0100

    Simplify ErrorController annotations

--- src/main/java/si/ferbisek/snackbox/controller/ErrorController.java
+++ src/main/java/si/ferbisek/snackbox/controller/ErrorController.java
@@ -4,13 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import si.ferbisek.snackbox.dto.ApiErrorResponse;
 
-@RestController
-@ControllerAdvice
+@RestControllerAdvice
 @Slf4j
 public class ErrorController {
 
