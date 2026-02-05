9f9f033a33ec4cbe1ed55d833caa6e7f2256bb26
Goran Ferbišek <goran.ferbisek@gmail.com>
2026-02-05 19:48:31 +0100

    Implement all CRUD methods for categories

--- src/main/java/si/ferbisek/snackbox/controller/CategoryController.java
+++ src/main/java/si/ferbisek/snackbox/controller/CategoryController.java
@@ -6,12 +6,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import si.ferbisek.snackbox.dto.CategoryDto;
-import si.ferbisek.snackbox.dto.CreateCategoryRequest;
 import si.ferbisek.snackbox.entity.Category;
 import si.ferbisek.snackbox.service.CategoryService;
 import si.ferbisek.snackbox.mappers.CategoryMapper;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/api/v1/categories")
@@ -30,16 +30,34 @@ public class CategoryController {
         return ResponseEntity.ok(categories);
     }
 
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<CategoryDto> findById(@PathVariable Long id) {
+        Optional<Category> foundCategory = categoryService.findById(id);
+
+        return foundCategory.map(categoryEntity -> {
+            CategoryDto categoryDto = categoryMapper.mapTo(categoryEntity);
+            return ResponseEntity.ok(categoryDto);
+        }).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
     @PostMapping
-    public ResponseEntity<CategoryDto> save(@Valid @RequestBody CreateCategoryRequest categoryRequest) {
-        Category categoryToCreate = categoryMapper.mapFrom(categoryRequest);
+    public ResponseEntity<CategoryDto> saveCategory(@Valid @RequestBody CategoryDto categoryDto) {
+        Category categoryToCreate = categoryMapper.mapFrom(categoryDto);
         Category savedCategory = categoryService.save(categoryToCreate);
 
         return new ResponseEntity<>(categoryMapper.mapTo(savedCategory), HttpStatus.CREATED);
     }
 
+    @PostMapping(path = "/{id}")
+    public ResponseEntity<CategoryDto> updateCategory(@PathVariable Long id, @Valid @RequestBody CategoryDto categoryDto) {
+        Category categoryToUpdate = categoryMapper.mapFrom(categoryDto);
+        Category updatedCategory = categoryService.update(id, categoryToUpdate);
+
+        return ResponseEntity.ok(categoryMapper.mapTo(updatedCategory));
+    }
+
     @DeleteMapping(path = "/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteCategory(@PathVariable Long id) {
         categoryService.delete(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
